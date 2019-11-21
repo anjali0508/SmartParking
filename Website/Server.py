e4edf38e61cfa74d9c59e73b53b4650e90658f88
Ananya <ananyasheshu@gmail.com>
2019-11-21 09:51:11 +0530

    Added server endpoints

--- Website/Server.py
+++ Website/Server.py
@@ -1,6 +1,13 @@
-from flask import Flask, make_response, jsonify, abort
+from flask import Flask, make_response, jsonify, abort, request
 import pyrebase
 import FirebaseConfig.Configure as firebase
+
+from Database.IDGenerator import ID as IDgen
+IDgen = IDgen()
+IDgen.setVisitorID()
+IDgen.setResidentVehicleID()
+IDgen.setResidentID()
+IDgen.setParkingSlotID()
 
 # Reference to firebese databse
 dbRef = firebase.dbRef
@@ -9,15 +16,20 @@ app = Flask(__name__)
 
 
 @app.errorhandler(404)
-def not_found(error):
+def notFound(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def badRequest(error):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
 @app.route("/")
 def index():
     return "Hello World"
 
-
+# Change id to string
 @app.route("/Resident/<int:id>/Profile", methods=["GET"])
 def getResident(id):
     residentDetails = dbRef.child("Residents").order_by_child(
@@ -29,9 +41,9 @@ def getResident(id):
     abort(404)
 
 
-@app.route("/Resident/<int:id>/home", methods=["GET"])
+@app.route("/Resident/<string:id>/home", methods=["GET"])
 def home(id):
-    id = str(id)
+    # id = str(id)
     # Get vehicle details
     vehicles = []
     vehicleDetails = dbRef.child("ResidentVehicles").order_by_child(
@@ -49,6 +61,55 @@ def home(id):
         del details["FlatNo"]
         visitors.append(details)
     return jsonify({"Vehicles": vehicles, "VisitoVehicles": visitors})
+
+
+@app.route("/Resident/<string:id>/AddVisitor", methods=["POST"])
+def addVisitor(id):
+    if not request.json or not "VehicleNo" in request.json:
+        abort(400)
+    visitor = {
+        "AllottedSlot": "",
+        "Arrived": False,
+        "Departed": False,
+        "FlatNo": id,
+        "Name": request.json.get("Name", ""),
+        "VehicleNo": request.json["VehicleNo"]
+    }
+
+    visitorID = IDgen.getNextVisitorID()
+    dbRef.child("Visitors").child(visitorID).set(visitor)
+    return jsonify({"Success": True}, 201)
+
+
+@app.route("/Resident/<string:id>/DeleteVisitor", methods=["POST"])
+def deleteVisitor(id):
+    if not request.json or not "VehicleNo" in request.json:
+        abort(400)
+    vehicleNo = request.json["VehicleNo"]
+    details = dbRef.child("Visitors").order_by_child(
+        "VehicleNo").equal_to(vehicleNo).get()
+    for x in details:
+        visitorID = x.key()
+        flatNo = x.val()["FlatNo"]
+        break
+    if flatNo == id:
+        dbRef.child("Visitors").child(visitorID).remove()
+        return jsonify({"Success": True}, 201)
+    return jsonify({"Success": False, "Message": "Invalid Vehicle Number"}, 201)
+
+# Change id to string
+@app.route("/Resident/<int:id>/ChangePassword", methods=["POST"])
+def changePassword(id):
+    if not request.json or not "Password" in request.json:
+        abort(400)
+    # Hash later
+    password = request.json["Password"]
+    details = dbRef.child("Residents").order_by_child(
+        "FlatNo").equal_to(id).get()
+    for x in details:
+        ID = x.key()
+    dbRef.child("Residents").child(ID).update({"Password": password})
+    return jsonify({"Success": True}, 201)
 
 
 if __name__ == '__main__':
