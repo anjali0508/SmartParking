cc8774946e58f3fc581483a4d61476f40a6a1761
Ananya <ananyasheshu@gmail.com>
2020-01-15 19:17:15 +0530

    Refactored code

--- RaspberryPi/DatabaseQuery.py
+++ RaspberryPi/DatabaseQuery.py
@@ -1,67 +1,49 @@
 import pyrebase
 import FirebaseConfig.Configure as firebase
 
+
 # Reference to firebese databse
 dbRef = firebase.dbRef
 
-
 def getFreeVisitorSlot():
-    # Finds an empty slot for visitor parking
-    # Input: Nil
-    # Output: Slot Number if free slots are available, else ''
-
     freeSlots = dbRef.child("ParkingSlots").order_by_child(
         "Type").equal_to("Visitor").order_by_child("Allotted").equal_to(False).get()
-
-    for freeSlot in freeSlots:
+    try:
+        freeSlot = freeSlots[0]
         slotIndex = freeSlot.key()
         slotNumber = freeSlot.val()["SlotNo"]
         # Update database
         dbRef.child("ParkingSlots").child(slotIndex).update({"Allotted": True})
         return slotNumber
-    return ""
+    except IndexError: # No free slots
+        return ""
 
 
 def verifyResident(licensePlateNumber):
-    # Checks whether a given License Plate NUmber belongs to a resident
-    # Input: License Plate Number
-    # Output: if resident: (True, parking slot)
-    #         else: (False, '')
-
-    residentDetails = dbRef.child("ResidentVehicles").order_by_child(
+    residents = dbRef.child("ResidentVehicles").order_by_child(
         "VehicleNo").equal_to(licensePlateNumber).get()
-
-    # This query returns a single resident if number exists. Obtain slot number and return
-    # If the number doent exist in database, returned object is empty
-
-    for resident in residentDetails:
-        allottedSlot = resident.val()["AllottedSlot"]
+    try:
+        allottedSlot = residents[0].val()["AllottedSlot"]
         return (True, allottedSlot)
-    return (False, "")
+    except IndexError: # Not a resident
+        return (False, "")  
 
 
 def verifyAndAllotVisitor(licensePlateNumber):
-    # Checks whether a given License Plate NUmber belongs to a registered visitor
-    # If true, allocates a parking slot
-    # Input: License Plate Number
-    # Output: if visitor registered:
-    #             if free slot available: (True, parking slot)
-    #             else: (True, '')
-    #         else: (False, '')
-
-    visitorDetails = dbRef.child("Visitors").order_by_child(
+    visitors = dbRef.child("Visitors").order_by_child(
         "VehicleNo").equal_to(licensePlateNumber).get()
-
-    for visitor in visitorDetails:
+    try:
+        visitor = visitors[0]
         visitorKey = visitor.key()
         updateInfo = {}
         updateInfo["Arrived"] = True
-        allottedSlot = getFreeVisitorSlot()
+        allottedSlot = getFreeVisitorSlot() # Returns "" if no slots available
         updateInfo["AllottedSlot"] = allottedSlot
         # Update the visitor details
         dbRef.child("Visitors").child(visitorKey).update(updateInfo)
         return (True, allottedSlot)
-    return (False, "")
+    except IndexError: # Not a visitor
+        return (False, "")
 
 def deleteVisitor(licensePlateNumber):
     details = dbRef.child("Visitors").order_by_child("VehicleNo").equal_to(licensePlateNumber).get()
@@ -70,7 +52,7 @@ def deleteVisitor(licensePlateNumber):
         slot = details[0].val()["AllottedSlot"]
         dbRef.child("Visitors").child(key).remove()
         freeSlot(slot)
-    except IndexError:
+    except IndexError: # Record not present in visitor table i.e., is a resident
         pass
 
 def freeSlot(slot):
@@ -81,11 +63,9 @@ def freeSlot(slot):
 
 if __name__ == "__main__":
     print(verifyResident("KA09A1111"))
-    # print(verifyResident("KA09A1122"))
-    # print(verifyAndAllotVisitor(("KA09A2212")))
-    # print(verifyAndAllotVisitor(("KA05P9999")))
-    # print(verifyAndAllotVisitor(("KA09A2222")))
-    # print(verifyAndAllotVisitor(("KA09A2212")))
-    # print(getFreeVisitorSlot())
-    # freeSlot("V22")
-    # deleteVisitor("lol")
+    print(verifyResident("KA09A1122"))
+    print(verifyAndAllotVisitor(("KA47P0090")))
+    print(verifyAndAllotVisitor(("KA05P9999")))
+    print(getFreeVisitorSlot())
+    freeSlot("V01")
+    deleteVisitor("lol")
